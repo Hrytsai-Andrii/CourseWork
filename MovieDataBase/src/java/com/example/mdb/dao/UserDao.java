8b98f348ba0a21a7bfad2ba9b08b82ec81defcb6
Hrytsai-Andrii <144918768+Hrytsai-Andrii@users.noreply.github.com>
2024-06-04 20:21:15 +0300

    Delete MovieDataBase directory

--- MovieDataBase/src/java/com/example/mdb/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.mdb.dao;
-
-import com.example.mdb.model.User;
-
-public interface UserDao extends AbstractDao<User> {
-
-    User getByLogin(String login);
-}
--- MovieDataBase/src/java/com/example/mdb/services/MovieSortCriteria.java
+++
@@ -1,9 +0,0 @@
-package com.example.mdb.services;
-
-public enum MovieSortCriteria {
-    OLD_FIRST,
-    NEW_FIRST,
-    BY_TITLE,
-    BY_LIKES,
-    BY_COMMENTS;
-}
